9dd24a30ff3bfc137b19f666c7b223f09364a61e
TzuHanLiang <crocodilestear@gmail.com>
2021-05-30 17:15:32 +0800

    feat: add all pub files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,15 +10,15 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")))
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminData.router);
 app.use("/", shopRoutes);
 
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render('404');
+  res.status(404).render('404', {docTitle: '404'});
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const path = require("path");
-
-const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-    // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product');
+    res.render('add-product',{docTitle: 'Add Product'});
 });
 
 router.post("/add-product", (req, res, next) => {
