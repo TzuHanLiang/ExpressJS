38d1191d607bd23f80341a9369a446071b3639c5
TzuHanLiang <crocodilestear@gmail.com>
2021-05-30 18:26:40 +0800

    feat: add layout

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.render('shop',{products: adminData.products, docTitle: 'Shop'});
+    res.render('shop',{products: adminData.products, docTitle: 'Shop', path: 'shop'});
 });
 
 module.exports = router;
